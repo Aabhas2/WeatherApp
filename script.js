e425e00cfb5d6e229eba277f64952dcf8c2ba6b7
Aabhas2 <sharma2.aabhas@gmail.com>
2024-10-29 23:38:36 +0530

    WeatherApp Complete

--- script.js
+++ script.js
@@ -1,34 +1,89 @@
-const apiKey = "e9367c5d3f54724882733114b3246654";
+//state
+let currCity = "London";
+let units = "metric";
 
-//function to fetch and display the weather data
-function getWeather() {
-    //Get the city name entered by the user
-    const city = document.getElementById("city").value;
+//Selectors
+let city = document.querySelector(".weather__city");
+let datetime = document.querySelector(".weather__datetime");
+let weather__forecast = document.querySelector(".weather__forecast");
+let weather__temperature = document.querySelector(".weather__temperature");
+let weather__icon = document.querySelector(".weather__icon");
+let weather__minmax = document.querySelector(".weather__minmax");
+let weather__realfeel = document.querySelector(".weather__realfeel");
+let weather__humidity = document.querySelector(".weather__humidity");
+let weather__wind = document.querySelector(".weather__wind");
+let weather__pressure = document.querySelector(".weather__pressure");
 
-    //OpenWeatherMap API URL with city name and API key 
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric';
-    //Fetch weather data 
 
-    fetch(url)
-        .then(response => {
-            if(!response.ok) {
-                throw new Error("City not found");
-            }
-            return response.json();
-        })
-        .then(data => {
-            //Extract and display weather information 
-            const weatherOutput = document.getElementById("weather-output");
-            weatherOutput.innerHTML =  `
-                <h2>Weather in ${data.name}</h2> 
-                <p>Temperature: ${data.main.temp}°C</p>
-                <p>Feels Like: ${data.main.feels_like}°C</p>
-                <p>Humidity: ${data.main.humidity}%</p>
-                <p>Weather: ${data.weather[0].description}</p>
-             `;
-        })
-        .catch(erro =>{
-            //Display error message if city is not found or other issues 
-            document.getElementById("weather-output").innerHTML = `<p>${error.message}</p>`;
-        });
+//search 
+document.querySelector(".weather__search").addEventListener('submit',e=>{
+    let search = document.querySelector(".weather__searchform");
+    //prevent default action 
+    e.preventDefault();
+    //change current city
+    currCity = search.value;
+    //get weather forecast
+    getWeather();
+    //clearform
+    search.value = ""
+})
+
+//units
+document.querySelector("weather_unit_celsius").addEventListener('click',()=>{
+    if(units!=="metric"){
+        //change to metric
+        unit = "metric"
+        //get the weather forecast 
+        getWeather();
+    }
+})
+
+document.querySelector("weather_unit_farenheit").addEventListener('click',()=>{
+    if(units!=="imperial"){
+        //change to imperial
+        unit = "imperial"
+        //get the weather forecast 
+        getWeather();
+    }
+})
+
+function convertTimeStamp(timestamp,timezone){
+    const date = new Date(timestamp*1000); //adjust timestamp with timezone 
+
+    const options ={
+        weekday:"long",
+        day:"numeric",
+        month: "long",
+        year:"numeric",
+        hour:"numeric",
+        minute:"numeric",
+        hour12:true,
+    };
+    return date.toLocaleString("en-US",{...options,timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone});
 }
+
+//Convert country code to name 
+function convertCountryCode(country){
+    let regionNames = new Intl.DisplayNames(["en"],{type:"region"});
+    return regionNames.of(country)
+}
+
+function getWeather() {
+    const apiKey = 'e9367c5d3f54724882733114b3246654';
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currCity}&appid=${apiKey}&units=${units}`).then(res=>res.json()).then(data=>{
+        console.log(data)
+        city.innerHTML = `${data.name},${convertCountryCode(data.sys.country)}`
+        datetime.innerHTML = convertTimeStamp(data.dt,data.timezone);
+        weather__forecast.innerHTML = `<p>${data.weather[0].main}`
+        weather__temperature.innerHTML = `${data.main.temp.toFixed()}&#176`
+        weather__icon.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png/>`
+        weather__minmax.innerHTML = `<p>Min:${data.main.temp_min.toFixed()}&#176</p>
+        <p>Max:${data.main.temp_max.toFixed()}&#176</p>`
+        weather__realfeel.innerHTML = `${data.main.feels_like.toFixed()}&#176`
+        weather__humidity.innerHTML = `${data.main.humidity}%`
+        weather__wind.innerHTML = `${data.wind.speed}${units==="imperial"?"mph":"m/s"}`
+        weather__pressure.innerHTML = `${data.main.pressure}hPa`
+    })
+}
+
+document.body.addEventListener('load',getWeather())
